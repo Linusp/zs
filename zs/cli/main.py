b57cd530af143dff57d58d40d96ee6d21ba7e57f
Linusp <linusp1024@gmail.com>
2021-02-10 11:23:34 +0800

    add `export-qieman` and `parse-qieman`

--- zs/cli/main.py
+++ zs/cli/main.py
@@ -13,6 +13,7 @@ from zs.consts import (
     SETUP_CFG,
     IGNORE_FILE_TEMPLATE,
 )
+from zs.qieman import QiemanExporter
 
 
 dictConfig({
@@ -128,6 +129,107 @@ def init_pyrepo(repo_name, python_version):
     fout = open(os.path.join(repo_name, '.gitignore'), 'w')
     fout.write(IGNORE_FILE_TEMPLATE)
     fout.close()
+
+
+@main.command("export-qieman")
+@click.option("-c", "--config-file", required=True)
+@click.option("--asset-id", required=True)
+@click.option("-o", "--outfile", required=True)
+def export_qieman_orders(config_file, asset_id, outfile):
+    """导出且慢订单记录"""
+    with open(config_file) as f:
+        config = json.load(f)
+        exporter = QiemanExporter(**config)
+        orders = exporter.list_orders(asset_id)
+
+    with open(outfile, 'w') as fout:
+        for order in orders:
+            line = json.dumps(order, ensure_ascii=False, sort_keys=True)
+            print(line, file=fout)
+
+
+@main.command("parse-qieman")
+@click.option("-i", "--infile", required=True)
+@click.option("-o", "--outfile", required=True)
+def parse_qieman_orders(infile, outfile):
+    """解析且慢订单记录为 csv 格式"""
+    import csv
+    from datetime import datetime
+
+    with open(infile) as fin, open(outfile, 'w') as fout:
+        pattern = re.compile(r'再投资份额(\d+\.\d+)份')
+        writer = csv.writer(fout, delimiter='\t', quoting=csv.QUOTE_ALL)
+        writer.writerow([
+            '账户', '名称', '代码', '类型', '时间', '价格',
+            '份额', '金额', '手续费', '资金来源', '资金去向'
+        ])
+        account = None
+        for line in fin:
+            item = json.loads(line)
+            if not account:
+                account = item['umaName']
+
+            if item['capitalAccountName'] == '货币三佳':
+                if item['uiOrderCodeName'].find('盈米宝') < 0 or item['orderStatus'] != 'SUCCESS':
+                    continue
+
+                fund_code = item['po']['poCode']
+                fund_name = item['po']['poName']
+                action, buy_from, sell_to = 'unknown', '', ''
+                if item['payStatus'] == '2':
+                    action = 'buy'
+                    buy_from = '盈米宝'
+                elif item['payStatus'] == '0':
+                    action = 'sell'
+                    sell_to = '盈米宝'
+
+                value = 1.0
+                fee = item['totalFee']
+                count = money = item['uiAmount']
+                order_time = datetime.fromtimestamp(item['acceptTime'] / 1000)
+                writer.writerow([
+                    account, fund_name, fund_code, action, order_time,
+                    f'{value:.4f}', f'{count:.4f}', f'{money:.4f}', f'{fee:.4f}',
+                    buy_from, sell_to
+                ])
+            elif item['hasDetail']:
+                if item['orderStatus'] != 'SUCCESS':
+                    continue
+
+                for order in item['compositionOrders']:
+                    value = order['nav']
+                    fee = order['fee']
+                    order_time = datetime.fromtimestamp(order['acceptTime'] / 1000)
+                    count = order['uiShare']
+                    money = order['uiAmount']
+                    action, buy_from, sell_to = 'unknown', '', ''
+                    if order['payStatus'] == '2':
+                        action = 'buy'
+                        buy_from = '盈米宝'
+                    elif order['payStatus'] == '0':
+                        action = 'sell'
+                        sell_to = '盈米宝'
+
+                    fund_code = order['fund']['fundCode']
+                    fund_name = order['fund']['fundName']
+                    writer.writerow([
+                        account, fund_name, fund_code, action, order_time,
+                        f'{value:.4f}', f'{count:.4f}', f'{money:.4f}', f'{fee:.4f}',
+                        buy_from, sell_to
+                    ])
+            elif item['uiOrderDesc'].find('再投资') >= 0:
+                fee = 0
+                order_time = datetime.fromtimestamp(item['acceptTime'] / 1000)
+                count = float(pattern.findall(item['uiOrderDesc'])[0])
+                money = item['uiAmount']
+                value = float(money) / float(count)
+                action = 'reinvest'
+                fund_code = item['fund']['fundCode']
+                fund_name = item['fund']['fundName']
+                writer.writerow([
+                    account, fund_name, fund_code, action, order_time,
+                    f'{value:.4f}', f'{count:.4f}', f'{money:.4f}', f'{fee:.4f}', '', ''
+                ])
 
 
 if __name__ == '__main__':
